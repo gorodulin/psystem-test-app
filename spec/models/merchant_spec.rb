97b9f516c5b253a349af841446fc6f1a05ea4250
Vladimir Gorodulin <ru.hostmaster@gmail.com>
2020-06-28 17:12:07 +0300

    Merchant with specs

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+
+  it "can be deleted only if there're no related transactions" do
+    transaction = FactoryBot.create(:authorize_transaction)
+    merchant = transaction.merchant
+    expect {merchant.destroy!}.to raise_error(ActiveRecord::InvalidForeignKey)
+    merchant.transactions.destroy_all
+    expect {merchant.destroy!}.not_to raise_error
+  end
+
 end
